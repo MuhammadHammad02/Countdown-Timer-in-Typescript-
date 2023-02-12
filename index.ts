f23322d7693687e145c37426b8a92158e2833736
Muhammad Hammad <108385539+MuhammadHammad02@users.noreply.github.com>
2023-02-12 16:05:12 +0500

    Countdown Timer

---
+++ index.ts
@@ -0,0 +1,30 @@
+import inquirer from "inquirer";
+
+const endDate= '2023-02-13'
+
+const intervalId =setInterval(()=>{
+    const distance = new Date(endDate).getTime()-new Date().getTime()
+    if(distance < 0) {
+        clearInterval(intervalId);
+        console.log("Countdown Ended");
+        process.exit(0)
+    }
+    else {
+        console.clear()
+        console.log(`Time left untile ${endDate}: ${distance/1000} seconds`)
+
+    }
+
+},1000)
+
+inquirer.prompt([{
+    type: "confirm",
+    name: "stop",
+    message: "Do you want to stop countdown"
+}]).then((answers) =>{
+    if(answers.stop){
+        clearInterval(intervalId);
+        console.log("countdown stopped");
+        process.exit(0)
+    }
+});
